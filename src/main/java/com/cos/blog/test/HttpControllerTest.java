06125f31e746484a9881a63d9ebd7d612f671399
yooseek <y1212ks@naver.com>
2021-11-27 16:01:59 +0900

    model

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -13,9 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HttpControllerTest {
+	private static final String TAG = "HttpControllerTest";
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG+" : getter :" + m.getUsername());
+		m.setUsername("See");
+		System.out.println(TAG+" : setter : " + m.getUsername());
+		return "lombok Test 완료";
+	}
+	
 	//http://localhost:8787/http/get(select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) { //자동으로 멤버클래스 생성(MessageConverter)
+		System.out.println(TAG+"getter" + m.getId());
+		m.setId(5000);
+		System.out.println(TAG+"setter" + m.getId());
 		return "get 요청 : " +m.getId()+","+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
 	//http://localhost:8787/http/post(insert)
