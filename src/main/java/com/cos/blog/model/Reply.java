d22f9c3154ea28b207c46c959a73dd3ae357da8e
yooseek <y1212ks@naver.com>
2021-11-28 19:09:52 +0900

    RoleType 및 Repository 설정

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -1,5 +1,45 @@
 package com.cos.blog.model;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Reply {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private int id;
+	
+	@Column(nullable = false,length=200)
+	private String content;
+	
+	@ManyToOne   // 여러개의 답글 - 하나의 게시글
+	@JoinColumn(name = "boardid")
+	private Board board;
+	
+	@ManyToOne   // 여러개의 답글 - 하나의 유저
+	@JoinColumn(name = "userid")
+	private User user;
+	
+	@CreationTimestamp
+	private Timestamp creatDate;
 
 }
---
+++ src/main/java/com/cos/blog/model/RoleType.java
@@ -0,0 +1,7 @@
+package com.cos.blog.model;
+
+public enum RoleType {
+	// domain 처리할때 사용한다 
+	User,Admin,Manager
+
+}
